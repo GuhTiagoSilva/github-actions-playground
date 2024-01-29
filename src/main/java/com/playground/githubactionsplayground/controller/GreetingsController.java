37c7d2edb84bc38e165b4cba1da44d0462bb5ac1
Gustavo Silva <Gustavo_Silva@McKinsey.com>
2024-01-29 15:51:32 -0300

    Implementing unit tests

--- src/main/java/com/playground/githubactionsplayground/controller/GreetingsController.java
+++ src/main/java/com/playground/githubactionsplayground/controller/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.playground.githubactionsplayground.controller;
 
+import com.playground.githubactionsplayground.dto.MessageDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +13,14 @@ public class GreetingsController {
 
     @GetMapping("/hello")
     @ResponseStatus(HttpStatus.OK)
-    public String hello() {
-        return "Hello World";
+    public MessageDto hello() {
+        return new MessageDto("Hello World");
     }
 
     @GetMapping("/goodbye")
     @ResponseStatus(HttpStatus.OK)
-    public String goodbye() {
-        return "Goodbye";
+    public MessageDto goodbye() {
+        return new MessageDto("Goodbye");
     }
 
 }
